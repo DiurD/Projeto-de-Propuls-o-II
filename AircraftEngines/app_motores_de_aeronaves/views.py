8b8a97a8a74d5359d46f78de3298280ee8d03b8c
Diego Martins <dmgp1302@gmail.com>
2023-09-22 11:02:42 -0300

    initial methods implementation

--- AircraftEngines/app_motores_de_aeronaves/views.py
+++ AircraftEngines/app_motores_de_aeronaves/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import resolve
+from app_motores_de_aeronaves import Prop2
 
 # Create your views here.
 
@@ -10,3 +11,4 @@ def motores(request):
     motor = request.resolver_match.url_name
     url = motor + '/home_'+motor+'.html'
     return render(request,url)
+
