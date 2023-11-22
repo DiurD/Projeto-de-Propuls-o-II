755b09aa96380bc103e4ca395742d0abb231d52a
Diego Martins <dmgp1302@gmail.com>
2023-11-21 22:08:46 -0300

    Site funfando

--- AircraftEngines/AircraftEngines/urls.py
+++ AircraftEngines/AircraftEngines/urls.py
@@ -17,13 +17,26 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 from app_motores_de_aeronaves import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # rota (ex: prop2.com/pagina1 -> "pagina1/"), view responsável e nome de referência
-    path('',views.home,name='home'),
-    path('turbojet/',views.motores,name='turbojet'),
-    path('turbofan/',views.motores,name='turbofan'),
-    path('turboprop/',views.motores,name='turboprop'),
-    path('ramjet/',views.motores,name='ramjet')
+
+    
+    # CÓDIGO OBSOLETO ABAIXO (PRIMEIRA VERSÃO)
+    # path('',views.home,name='home'),
+    # path('turbojet/',views.motores,name='turbojet'),
+    # path('turbofan/',views.motores,name='turbofan'),
+    # path('turboprop/',views.motores,name='turboprop'),
+    #path('ramjet/',views.motores,name='ramjet'),
+
+    path('',views.index,name='index'),
+    path('sobre',views.teste,name='sobre'),
+    path('instrucoes',views.teste,name='instrucoes'),
+    path('resultados',views.teste,name='resultados'),
+    path('bibliografia',views.teste,name='bibliografia'),
+    
 ]
--- AircraftEngines/app_motores_de_aeronaves/views.py
+++ AircraftEngines/app_motores_de_aeronaves/views.py
@@ -4,8 +4,18 @@ from app_motores_de_aeronaves.templates import Prop2
 
 # Create your views here.
 
+def index(request):
+    template = "Site_arquivos/index.html"
+    return render(request,template)
+
+def teste(request):
+    escolha = request.resolver_match.url_name
+    url = "Site_arquivos/" + escolha + '.html'
+    return render(request,url)
+
 def home(request):
     return render(request,'propulsao2/home.html')
+
 
 def motores(request):
     motor = request.resolver_match.url_name
