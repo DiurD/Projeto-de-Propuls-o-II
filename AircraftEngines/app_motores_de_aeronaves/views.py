27cc92c4d183f8a45ae818307f64e60e35a939a5
Diego Martins <dmgp1302@gmail.com>
2023-11-28 09:56:25 -0300

    Teste2

--- AircraftEngines/app_motores_de_aeronaves/views.py
+++ AircraftEngines/app_motores_de_aeronaves/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app_motores_de_aeronaves.templates import Prop2
 from app_motores_de_aeronaves.models import atmos
+import re
 
 
 def index(request):
@@ -18,15 +19,12 @@ def results(request):
     # Todos os dados são recebidos via POST no objeto request.POST
     
     #print("\n \n")
-    #for i in range(len(request.POST.getlist('nome'))):
-    #    if request.POST.getlist('nome')[i]:
-    #        nome = request.POST.getlist('nome')[i]
-    #        tipo = request.POST['motor']
-    #        ideal = request.POST['ideal']
-    #        on_design = request.POST['onDesign']
-
-    # if request.POST['absoluto']:
-    #    diametros = [float(0)]*10
+    for i in range(len(request.POST.getlist('nome'))):
+        if request.POST.getlist('nome')[i]:
+            nome = request.POST.getlist('nome')[i]
+            tipo = request.POST['motor']
+            ideal = request.POST['ideal']
+            on_design = request.POST['onDesign']
 
     match request.POST['motor']:
         case 'ramjet':
@@ -60,3 +58,28 @@ def motores(request):
     motor = request.resolver_match.url_name
     url = motor + '/home_'+motor+'.html'
     return render(request,url)
+
+def diametros(request):
+
+    diametros = [float(0)]*10
+
+    
+
+    for key in request.POST:
+
+        if key in ['d0','d1','d2','d3','d4','d5','d6','d7','d8','d9']:
+
+            num = int(re.findall("[0-9]",key)[0])
+
+            if request.POST['absoluto']:
+                if request.POST[key]:
+                    diametros[num] = request.POST[key]
+                else:
+                    diametros[num] = float(0)
+            else:
+                if request.POST[key]:
+                    diametros[num] = request.POST[key]*request.POST['diametro-nominal']
+                else:
+                    diametros[num] = float(0)
+
+        
