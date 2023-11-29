5b2f5f9bdb4b19dd8de1d2a400b6b5a3ff052f76
Vinícius <viny272@gmail.com>
2023-11-29 20:43:10 -0300

    update

--- AircraftEngines/app_motores_de_aeronaves/static/js/chart.js
+++ AircraftEngines/app_motores_de_aeronaves/static/js/chart.js
@@ -2,13 +2,12 @@
   const { datum } = window;
   console.log(datum);
 
-  const labels = datum.Pos;
+  const labels = datum.Section;
   const data = {
     labels: labels,
     datasets: [
       {
         label: "Pressão [Pa]",
-        hidden: true,
         data: datum.P,
         borderColor: "red",
         backgroundColor: "red",
@@ -17,11 +16,80 @@
       {
         label: "Temperatura [K]",
         data: datum.T,
+        hidden: true,
         borderColor: "blue",
         backgroundColor: "blue",
         yAxisID: "y2",
       },
+      {
+        label: "Mach",
+        hidden: true,
+        data: datum.Mach,
+        borderColor: "purple",
+        backgroundColor: "purple",
+        yAxisID: "y3",
+      },
+      {
+        label: "Diâmetro [m]",
+        hidden: true,
+        data: datum.D,
+        borderColor: "green",
+        backgroundColor: "green",
+        yAxisID: "y4",
+      },
+      {
+        label: "Área [m²]",
+        hidden: true,
+        data: datum.A,
+        borderColor: "orange",
+        backgroundColor: "orange",
+        yAxisID: "y5",
+      },
+      {
+        label: "Área ótima [m²]",
+        hidden: true,
+        data: datum.Aot,
+        borderColor: "pink",
+        backgroundColor: "pink",
+        yAxisID: "y6",
+      },
+      {
+        label: "Área/Área ótima [m²/m²]",
+        hidden: true,
+        data: datum.A_Aot,
+        borderColor: "gray",
+        backgroundColor: "gray",
+        yAxisID: "y7",
+      },
+      {
+        label: "Pressão total [Pa]",
+        hidden: true,
+        data: datum.Pt,
+        borderColor: "black",
+        backgroundColor: "black",
+        yAxisID: "y8",
+      },
+      {
+        label: "Pressão total [Pa]",
+        hidden: true,
+        data: datum.Pt,
+        borderColor: "yellow",
+        backgroundColor: "yellow",
+        yAxisID: "y9",
+      },
     ],
+  };
+
+  const plugin = {
+    id: "customCanvasBackgroundColor",
+    beforeDraw: (chart, args, options) => {
+      const { ctx } = chart;
+      ctx.save();
+      ctx.globalCompositeOperation = "destination-over";
+      ctx.fillStyle = options.color || "#99ffff";
+      ctx.fillRect(0, 0, chart.width, chart.height);
+      ctx.restore();
+    },
   };
 
   new Chart(document.getElementById("canvas"), {
@@ -35,12 +103,27 @@
       },
       stacked: false,
       plugins: {
+        customCanvasBackgroundColor: {
+          color: "white",
+        },
         title: {
           display: true,
-          text: "Resultados por seção",
+          text: "Resultados por seção - " + datum.Nome,
+          font: {
+            size: 30,
+          },
         },
       },
       scales: {
+        x: {
+          title: {
+            display: true,
+            text: "Seções do motor",
+            font: {
+              size: 15,
+            },
+          },
+        },
         y1: {
           type: "linear",
           display: true,
@@ -56,69 +139,110 @@
             drawOnChartArea: false, // only want the grid lines for one axis to show up
           },
         },
+        y3: {
+          type: "linear",
+          display: true,
+          position: "left",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y4: {
+          type: "linear",
+          display: true,
+          position: "right",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y5: {
+          type: "linear",
+          display: true,
+          position: "left",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y6: {
+          type: "linear",
+          display: true,
+          position: "right",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y7: {
+          type: "linear",
+          display: true,
+          position: "left",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y8: {
+          type: "linear",
+          display: true,
+          position: "right",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
+        y9: {
+          type: "linear",
+          display: true,
+          position: "left",
+          ticks: {
+            display: false,
+          },
+
+          // grid line settings
+          grid: {
+            drawOnChartArea: false, // only want the grid lines for one axis to show up
+          },
+        },
       },
     },
+    plugins: [plugin],
   });
 })();
 
-// (async function () {
-//   const DATA_COUNT = 7;
-//   const NUMBER_CFG = { count: DATA_COUNT, min: -100, max: 100 };
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("download-chart").addEventListener("click", download);
+});
 
-//   const labels = Utils.months({ count: 7 });
-//   const data = {
-//     labels: labels,
-//     datasets: [
-//       {
-//         label: "Dataset 1",
-//         data: Utils.numbers(NUMBER_CFG),
-//         borderColor: Utils.CHART_COLORS.red,
-//         backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-//         yAxisID: "y",
-//       },
-//       {
-//         label: "Dataset 2",
-//         data: Utils.numbers(NUMBER_CFG),
-//         borderColor: Utils.CHART_COLORS.blue,
-//         backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-//         yAxisID: "y1",
-//       },
-//     ],
-//   };
-
-//   new Chart(document.getElementById("canvas"), {
-//     type: "line",
-//     data: data,
-//     options: {
-//       responsive: true,
-//       interaction: {
-//         mode: "index",
-//         intersect: false,
-//       },
-//       stacked: false,
-//       plugins: {
-//         title: {
-//           display: true,
-//           text: "Chart.js Line Chart - Multi Axis",
-//         },
-//       },
-//       scales: {
-//         y: {
-//           type: "linear",
-//           display: true,
-//           position: "left",
-//         },
-//         y1: {
-//           type: "linear",
-//           display: true,
-//           position: "right",
-
-//           // grid line settings
-//           grid: {
-//             drawOnChartArea: false, // only want the grid lines for one axis to show up
-//           },
-//         },
-//       },
-//     },
-//   });
-// })();
+var download = function () {
+  var link = document.createElement("a");
+  link.download = "grafico.png";
+  link.href = document.getElementById("canvas").toDataURL("image/png");
+  link.click();
+};
