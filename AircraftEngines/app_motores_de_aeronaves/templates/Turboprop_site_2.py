922e44c4d6e6779e855cb5d54bf158b9b751b49b
gabrielmlonde <gabrielmlonde2@gmail.com>
2023-12-11 12:51:27 -0300

    Turboprop_site_2 terminando espero

--- AircraftEngines/app_motores_de_aeronaves/templates/Turboprop_site_2.py
+++ AircraftEngines/app_motores_de_aeronaves/templates/Turboprop_site_2.py
@@ -14,28 +14,6 @@ class motor_turboprop_2:
         self.A=[float(0)]*11
         #self.airIntakes = intakes
 
-        #for i in range(len(self.D)):
-        #    if i == 1:
-        #        self.A[i] = (math.pi*self.D[i]**2)/4*self.airIntakes
-        #    else:
-        #        if self.D[i]==0 and i!=0:
-        #            self.D[i] = self.D[i-1]
-        #            self.A[i] = self.A[i-1]
-        #        else:
-        #            self.A[i] = (math.pi*self.D[i]**2)/4
-        # self.A[0] = self.A[1]-self.A[0]
-
-
-    #def __str__(self):
-        #string = "------------\nNome: {}".format(self.name)
-        # string = string+ "\nDiâmetro: {}".format(self.diameter)
-        #string = string+ "\nComprimento: {}".format(self.length)
-        # string = string+ "\nPeso: {}".format(self.weight)
-        #string += "\n°°°°°°°°°°°°°°°°°°°°"
-        #for i in range(0,len(self.D)):
-        #    string = string+ "\nDiâmetro e área da seção {}: {} [m] | {:.4f} [m²]".format(i,self.D[i],self.A[i])
-        #string += "\n°°°°°°°°°°°°°°°°°°°°"
-        #return string 
 
     def calcula_parametrico(self, gamma_c,gamma_t, cp_c , cp_t , hpr, Tt4, pi_c, tau_t, eta_prop,atmos:Prop2.AircraftEngines,ideal,m0_dot,pi_b=1.0,pi_d_max=1.0,pi_n=1.0,eta_b=1.0,eta_mL=1.0,eta_mH=1.0,eta_g=1.0,e_c=1.0,e_tH=1.0,e_tL=1.0):
         
@@ -56,39 +34,18 @@ class motor_turboprop_2:
         #Ms[3] = self.M3
         #Ms[4] = 1 #Página 562 Mattingly
         #Ms[5] = 1 #Página 562 Mattingly
-        # O resto dos Machs é definido com base no Mach na seção 9 calculado após o ciclo.
-
-
-# Não alterei essa parte debaixo comentada do "if ideal", se for um problema, me avise.
-        #if ideal:
-        #    P0_P9 = 1.0
-        #else:
-        #    while 'P0_P9' not in locals():
-        #        text = input("\n O fluxo é engasgado (choked)? ")
-        #        if re.search('(?i)^sim|^s|^1',text):
-        #            P0_P9 = float(1)
-        #        elif re.search('(?i)^não|^n|^nao|^2',text):
-        #            P0_P9 = float(input("Qual a razão de pressão P0/P9? "))
-        #        else:
-        #            print("Digite uma opção válida!\n")
             
         
        #output,tau_lambda,pi_r,  tau_r,  pi_d,  tau_d,  pi_c,  tau_c,  pi_b,  tau_b,  pi_tH, tau_tH, pi_tL, tau_tL, pi_n,  tau_n,  P0_P9,Pt9_P9,T9_Tt9,T9_T0,M9
         output,tau_lambda,pis[0],taus[0],pis[2],taus[2],pis[3],taus[3],pis[4],taus[4],pis[5],taus[5],pis[6],taus[6],pis[9],taus[9],P0_P9,Pt9_P9,T9_Tt9,T9_T0,M9 = atmos.real_turboprop(self.M0,Tt4,gamma_c,cp_c,gamma_t,cp_t,hpr,pi_d_max,pi_b,pi_n,pi_c,e_c,e_tH,e_tL,tau_t,eta_b,eta_mL,eta_mH,eta_g,eta_prop,m0_dot)
 
-        #f = output.get('f')
-        #air_comb = 1/f[0]
-        #output['AF ratio'] = [air_comb]
+
         output['Tau_lambda'] = [tau_lambda]
         output['P0/P9'] = [P0_P9]
         output['Pt9/P9'] = [Pt9_P9]
         output['T9/Tt9'] = [T9_Tt9]
         output['T9/T0'] = [T9_T0]
-        
-        #pis[2] = pis[1]
-        #taus[2] = taus[1]
-        #pis[7] = pis[6] = pis[5] =pis[4]
-        #taus[7] = taus[6] = taus[5] =taus[4]
+
 
 
         for i in range(len(secao)):
@@ -118,6 +75,10 @@ class motor_turboprop_2:
         'Tau':taus,
         'Pt [Pa]': Pts,
         'Tt [K]': Tts,
+        'Mach0': Ms[0],
+        'Mach9': Ms[10],
+        'T0 [K]': T0,
+        'P0 [Pa]': P0,
         }
 
         return output,saidas
@@ -127,26 +88,6 @@ class motor_turboprop_2:
         secao = [0,    2 ,  3  ,  4  , 4.5 ,  5  ,  8   ,9]
         datum = [0, 0.060,0.408,0.616,0.679,0.715,0.9498,1]
 
-        #while 'escolha' not in locals():
-        #        mudar = input("\n Deseja mudar as posições dos componentes em relação à entrada de ar? ")
-        #        if re.search('(?i)^sim|^s|^1',mudar):
-        #            escolha = input("\nDeseja mudar pelo datum ou posição total?\n1 - Datum\n2 - Posição Absoluta")
-        #            if re.search('(?i)^datum|^1',escolha):
-        #                for i in range(len(secao)):
-        #                    datum[i] = float(input(f"Valor do datum da seção {secao[i]}"))
-        #                    posicao[i] = datum[i]*self.length
-        #            elif re.search('(?i)^pos|^2',escolha):
-        #                for i in range(len(secao)):
-        #                    posicao[i] = float(input(f"Posição absoluta da seção {secao[i]}"))
-        #                    datum[i] = posicao[i]/self.length
-        #            else:
-        #                print("!!! DIGITE UM VALOR VÁLIDO !!!")
-
-        #        elif re.search('(?i)^não|^n|^nao|^2',mudar):
-        #            escolha = None
-        #            pass
-        #        else:
-        #            print("Digite uma opção válida!\n")
 
         output_Mattingly_REF= {}
         saida_REF = {}
@@ -190,18 +131,6 @@ class motor_turboprop_2:
         output_REF={'Parâmetros inseridos manualmente': ["Cálculo de seções não ocorreu"]}
         saida_REF={'Parâmetros inseridos manualmente': ["Cálculo de seções não ocorreu"]}
         
-        #print("\nA atmosfera de referência é a seguinte:\n")
-        #print(atmos_REF)
-        #print(f"\nCom velocidade de referência como Mach {self.M0}")
-        #self.altera_M0()
-
-        # M0_AT = float(input("\nQual o novo Mach para cálculo do off design? ")) #IMPORTANTES!!!!
-        # P0_P9_AT = float(input("\nQual o P0/P9 para cálculo do off design? "))
-        # Tt4_AT = float(input("\nQual o novo Tt4 [K] para cálculo do off design? "))
-
-        # print("\nCrie agora a nova atmosfera para simulação do off-design:\n") IMPORTANTE!!!
-        # atmos_AT = self.cria_atmos()
-        # print(atmos_AT)
         
         T0,P0,_,_ = atmos_AT.get_param()
 
@@ -209,29 +138,9 @@ class motor_turboprop_2:
         output_REF,saida_REF = self.calcula_parametrico(gamma_c,gamma_t, cp_c , cp_t , hpr, Tt4_R,pi_c_R, tau_t, eta_prop,atmos_REF,                  ideal,m0_dot_R,pi_b,pi_d_max,pi_n,eta_b,eta_mL,eta_mH,eta_g,e_c,e_tH,e_tL)
         
         if Pt9_P9_R == 1 and m0_dot_R ==1: #Que isso??????                                                                                                                                                                                                            #pi_b,pi_tH,pi_n,tau_tH,eta_c,eta_b,eta_tL, eta_mL, eta_g, eta_propmax, M0_R, T0_R, P0_R, m0_dot_R, tau_r_R, pi_r_R, Tt4_R, pi_d_R, pi_c_R, tau_c_R, pi_tL_R, tau_tL_R, M9_R, Pt9_P9_R
-            output,tau_lambda,pis[0],taus[0],pis[2],taus[2],pis[3],taus[3],pis[4],taus[4],pis[5],taus[5],pis[6],taus[6],pis[9],taus[9],P0_P9,Pt9_P9,T9_Tt9,T9_T0,M9 = atmos_AT.offdesign_turboprop(M0_AT, Tt4_AT, P0_P9_AT, gamma_c,cp_c,gamma_t,cp_t,hpr,pi_d_max,pi_b,pi_tH,pi_n,tau_tH,eta_c,eta_b,eta_tL, eta_mL, eta_g, eta_propmax,saida_REF['Mach'][0],saida_REF['T [K]'][0],saida_REF['P [Pa]'][0],output_REF['m0_dot'][0],saida_REF['Tau'][0],saida_REF['Pi'][0],saida_REF['Tt [K]'][4],saida_REF['Pi'][2],saida_REF['Pi'][3],saida_REF['Tau'][3],saida_REF['Pi'][5],saida_REF['Tau'][5],saida_REF['Mach'][10],output_REF['Pt9/P9'][0])
+            output,tau_lambda,pis[0],taus[0],pis[2],taus[2],pis[3],taus[3],pis[4],taus[4],pis[5],taus[5],pis[6],taus[6],pis[9],taus[9],P0_P9,Pt9_P9,T9_Tt9,T9_T0,M9 = atmos_AT.offdesign_turboprop(M0_AT, Tt4_AT, P0_P9_AT, gamma_c,cp_c,gamma_t,cp_t,hpr,pi_d_max,pi_b,pi_tH,pi_n,tau_tH,eta_c,eta_b,eta_tL, eta_mL, eta_g, eta_propmax,saida_REF['Mach0'][0],saida_REF['T0 [K]'][0],saida_REF['P0 [Pa]'][0],output_REF['m0_dot'][0],saida_REF['Tau'][0],saida_REF['Pi'][0],saida_REF['Tt [K]'][4],saida_REF['Pi'][2],saida_REF['Pi'][3],saida_REF['Tau'][3],saida_REF['Pi'][5],saida_REF['Tau'][5],saida_REF['Mach9'][0],output_REF['Pt9/P9'][0])
         else:                                                                                                                                                                                                                                                     #pi_b,pi_tH,pi_n,tau_tH,eta_c,eta_b,eta_tL, eta_mL, eta_g, eta_propmax, M0_R, T0_R, P0_R, m0_dot_R, tau_r_R, pi_r_R, Tt4_R, pi_d_R, pi_c_R, tau_c_R, pi_tL_R, tau_tL_R, M9_R, Pt9_P9_R                                                                                                                                                                                                                                                
             output,tau_lambda,pis[0],taus[0],pis[2],taus[2],pis[3],taus[3],pis[4],taus[4],pis[5],taus[5],pis[6],taus[6],pis[9],taus[9],P0_P9,Pt9_P9,T9_Tt9,T9_T0,M9 = atmos_AT.offdesign_turboprop(M0_AT, Tt4_AT, P0_P9_AT, gamma_c,cp_c,gamma_t,cp_t,hpr,pi_d_max,pi_b,pi_tH,pi_n,tau_tH,eta_c,eta_b,eta_tL, eta_mL, eta_g, eta_propmax, M0_R, T0_R, P0_R, m0_dot_R, tau_r_R, pi_r_R, Tt4_R, pi_d_R, pi_c_R, tau_c_R, pi_tL_R, tau_tL_R, M9_R, Pt9_P9_R)
-           
-        #P0_P9_REF = output_REF['P0/P9'][0]
-        
-        #while 'escolha' not in locals():
-        #    mudar = 'sim' #input("\nDeseja simular com os parâmetros de referência do ciclo on desing?  Caso não, insira-os manualmente\n")
-        #    if re.search('(?i)^sim|^s|^1|^on|design',mudar):
-        #        print("\nRealizando análise prévia do on design:\n")
-
-                
-                
-                
-                
-        #        escolha = True
-        #    elif re.search('(?i)^nao|^2|^n|^man|^manualmente|manual',mudar):
-        #        variables = re.split("\s",input("Por fim, insira os parâmetros de referência:\nM0_R, T0_R, P0_R, tau_r_R, pi_r_R, Tt4_R, pi_d_R, Pt9_P9_R, m0_R\n"))
-        #        M0_R = float(variables[0]); T0_R = float(variables[1]); P0_R = float(variables[2]); tau_r_R = float(variables[3]); pi_r_R = float(variables[4]); Tt4_R = float(variables[5]); pi_d_R = float(variables[6]); Pt9_P9_R = float(variables[7]); m0_R = float(variables[8])
-        #        
-        #        escolha = False
-        #    else:
-        #        print("!!! DIGITE UMA OPÇÃO VÁLIDA !!!")
 
 
         Ms = [float(1)]*11
@@ -244,7 +153,7 @@ class motor_turboprop_2:
        # Ms[5] = 1 #Página 562 Mattingly    
        # Ms[6] = Ms[7] = Ms[8] = Ms[9] = Ms[10] = M9 # Desconsiderar mudança de Mach após sair da turbina, ao longo do bocal de saída
         Ms[10] = M9
-        #
+        
         output['Tau_lambda'] = [tau_lambda]
         output['P0/P9'] = [P0_P9_AT]
         output['Pt9/P9'] = [Pt9_P9]
